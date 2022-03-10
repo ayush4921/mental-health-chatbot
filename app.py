fa9461a39df21db40d96e5bc3eafb8023e54fd8d
Ayush Garg <themoviechannel66@gmail.com>
2022-03-10 12:48:17 +0800

    added files

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ from train import train_with_corpus
 import os
 app = Flask(__name__)
 CORS(app)
-
+bot = create_bot()
+if not os.path.exists(os.path.join(os.getcwd(),'db.sqlite3')):
+    train_with_corpus(bot, 'chatterbot.corpus.english.greetings')
+    train_with_corpus(bot, 'chatterbot.corpus.english.botprofile')
+    train_with_corpus(bot, 'chatterbot.corpus.english.emotion')
+    train_with_corpus(bot, 'chatterbot.corpus.english.gossip')
+    train_with_corpus(bot, 'chatterbot.corpus.english.health')
+    train_with_corpus(bot, 'chatterbot.corpus.english.psychology')
+    train_with_csv(bot, './data/mental_health_faq.csv')
 @app.route("/", methods=["GET"])
 def return_home():
     return render_template('index.html')
@@ -16,15 +24,7 @@ def use_bot():
 
     question = request.get_json()["question"]
     print(question)
-    bot = create_bot()
-    if not os.path.exists(os.path.join(os.getcwd(),'db.sqlite3')):
-        train_with_corpus(bot, 'chatterbot.corpus.english.greetings')
-        train_with_corpus(bot, 'chatterbot.corpus.english.botprofile')
-        train_with_corpus(bot, 'chatterbot.corpus.english.emotion')
-        train_with_corpus(bot, 'chatterbot.corpus.english.gossip')
-        train_with_corpus(bot, 'chatterbot.corpus.english.health')
-        train_with_corpus(bot, 'chatterbot.corpus.english.psychology')
-        train_with_csv(bot, './data/mental_health_faq.csv')
+    
     response = get_response(bot,question)
     return {"response":str(response)}
 
