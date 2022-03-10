386c4263016dafb14fbd04fa2daa03fe268fe8e5
Ayush Garg <themoviechannel66@gmail.com>
2022-03-10 14:12:10 +0800

    added files

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, Markup
 from flask_cors import CORS
-from mental_health_chatbot import create_bot, get_response, train_with_csv, train_with_corpus
+from .mental_health_chatbot import create_bot, get_response, train_with_csv, train_with_corpus
 import os
 app = Flask(__name__)
 CORS(app)
